802ba5bc7807a3f16d0479ec42677aeda772cef8
Sloan Haywood <haywood.sloan@yahoo.com>
2017-04-26 19:57:55 -0400

    Formatting updates.

--- Netflix and Game/main.cpp
+++ Netflix and Game/main.cpp
@@ -15,17 +15,19 @@ UINT soundOption = s25ItemID;
 bool reqFullscreen = true;
 bool hasChangedVolume = false;
 
-struct mediaCommand {
+struct mediaCommand
+{
 	char *title;
 	UINT button;
 };
-const mediaCommand mediaCommands[] = { {"YouTube",'K'}, {"Netflix", VK_SPACE}, {"Hulu", VK_SPACE}, 
+
+const mediaCommand mediaCommands[] = { {"YouTube",'K'}, {"Netflix", VK_SPACE}, {"Hulu", VK_SPACE},
 	{"Spotify", 0}, {"Skype", 0}, {"VLC media player", VK_SPACE} };
 
-LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-
-	if (uMsg == shellCallback && lParam == WM_RBUTTONUP) {
-
+LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == shellCallback && lParam == WM_RBUTTONUP)
+	{
 		POINT p;
 		GetCursorPos(&p);
 
@@ -33,14 +35,16 @@ LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 		TrackPopupMenu(popupMenu, 0, p.x, p.y, 0, msgWindow, 0);
 		PostMessage(msgWindow, WM_NULL, 0, 0);
 	}
-	else if (uMsg == WM_COMMAND) {
-
-		if (LOWORD(wParam) == quitItemID) {
+	else if (uMsg == WM_COMMAND)
+	{
+		if (LOWORD(wParam) == quitItemID)
+		{
 			Shell_NotifyIcon(NIM_DELETE, &shellData);
 			UnhookWindowsHookEx(keyHook);
 			exit(0);
 		}
-		else if (LOWORD(wParam) == reqFSItemID) {
+		else if (LOWORD(wParam) == reqFSItemID)
+		{
 			reqFullscreen = !reqFullscreen;
 
 			MENUITEMINFO checkInfo = {};
@@ -52,7 +56,8 @@ LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 			checkInfo.fState ^= MFS_UNCHECKED;
 			SetMenuItemInfo(popupMenu, reqFSItemID, 0, &checkInfo);
 		}
-		else {
+		else
+		{
 			soundOption = LOWORD(wParam);
 
 			MENUITEMINFO checkInfo = {};
@@ -62,13 +67,15 @@ LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 			HMENU soundOptions = GetSubMenu(popupMenu, 0);
 			UINT count = GetMenuItemCount(soundOptions);
 
-			for (UINT i = 0; i < count; i++) {
-
-				if (GetMenuItemID(soundOptions, i) == soundOption) {
+			for (UINT i = 0; i < count; i++)
+			{
+				if (GetMenuItemID(soundOptions, i) == soundOption)
+				{
 					checkInfo.fState = MFS_CHECKED;
 					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
 				}
-				else {
+				else
+				{
 					checkInfo.fState = MFS_UNCHECKED;
 					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
 				}
@@ -79,14 +86,15 @@ LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-void changeFGWindowVolume() {
-
+void changeFGWindowVolume()
+{
 	char titleBuff[128];
 	HWND activeHWND = GetForegroundWindow();
 	GetWindowText(activeHWND, titleBuff, 128);
 
 	UINT count = sizeof(mediaCommands) / sizeof(mediaCommands[0]);
-	for (UINT i = 0; i < count; i++) {
+	for (UINT i = 0; i < count; i++)
+	{
 		if (strstr(titleBuff, mediaCommands[i].title) > 0) return;
 	}
 
@@ -108,24 +116,26 @@ void changeFGWindowVolume() {
 
 	int sessionCount;
 	sessionEnum->GetCount(&sessionCount);
-	for (int i = 0; i < sessionCount; i++) {
-
+	for (int i = 0; i < sessionCount; i++)
+	{
 		sessionEnum->GetSession(i, &sessionControl);
 		sessionControl->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&sessionControl2);
 
 		DWORD pid;
 		sessionControl2->GetProcessId(&pid);
-		if (activePid == pid) {
-
+		if (activePid == pid)
+		{
 			sessionControl->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&audioVolume);
 
-			if (soundOption == muteItemID) {
+			if (soundOption == muteItemID)
+			{
 				BOOL muted;
 
 				audioVolume->GetMute(&muted);
 				audioVolume->SetMute(!muted, 0);
 			}
-			else {
+			else
+			{
 				float volumeLevel;
 				float newVolumeLevel = (soundOption - 40000) / 100.0f;
 
@@ -146,16 +156,18 @@ void changeFGWindowVolume() {
 	mmDeviceEnum->Release();
 }
 
-BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam) {
-
+BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam)
+{
 	char titleBuff[128];
 	GetWindowText(hwnd, titleBuff, 128);
 
 	UINT count = sizeof(mediaCommands) / sizeof(mediaCommands[0]);
-	for (UINT i = 0; i < count; i++) {
-		if (strstr(titleBuff, mediaCommands[i].title) > 0) {
-
-			if (soundOption != dncItemID && !hasChangedVolume) {
+	for (UINT i = 0; i < count; i++)
+	{
+		if (strstr(titleBuff, mediaCommands[i].title) > 0)
+		{
+			if (soundOption != dncItemID && !hasChangedVolume)
+			{
 				changeFGWindowVolume();
 				hasChangedVolume = true;
 			}
@@ -177,8 +189,8 @@ BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam) {
 	return 1;
 }
 
-bool isActiveWindowFullscreen() {
-
+bool isActiveWindowFullscreen()
+{
 	HWND activeHwnd = GetForegroundWindow();
 	if (activeHwnd == GetShellWindow()) return 0;
 
@@ -197,22 +209,28 @@ bool isActiveWindowFullscreen() {
 		activeHwndRect.right >= activeMonitorInfo.rcMonitor.right - 1);
 }
 
-LRESULT CALLBACK keyHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
-
-	if (wParam == WM_KEYDOWN) {
+LRESULT CALLBACK keyHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (wParam == WM_KEYDOWN)
+	{
 
 		KBDLLHOOKSTRUCT *kbHookStruct = (KBDLLHOOKSTRUCT*)lParam;
-		if (kbHookStruct->vkCode == VK_MEDIA_PLAY_PAUSE) {
-			if (!reqFullscreen || isActiveWindowFullscreen()) {
+		if (kbHookStruct->vkCode == VK_MEDIA_PLAY_PAUSE)
+		{
+			if (!reqFullscreen || isActiveWindowFullscreen())
+			{
 				EnumWindows(EnumWindowProc, 0);
 				hasChangedVolume = false;
 			}
 		}
 	}
-	else if (wParam == WM_SYSKEYDOWN) {
+	else if (wParam == WM_SYSKEYDOWN)
+	{
 		KBDLLHOOKSTRUCT *kbHookStruct = (KBDLLHOOKSTRUCT*)lParam;
-		if (kbHookStruct->vkCode == VK_F6) {
-			if (!reqFullscreen || isActiveWindowFullscreen() && soundOption != dncItemID) {
+		if (kbHookStruct->vkCode == VK_F6)
+		{
+			if (!reqFullscreen || isActiveWindowFullscreen() && soundOption != dncItemID)
+			{
 				changeFGWindowVolume();
 			}
 		}
@@ -221,9 +239,9 @@ LRESULT CALLBACK keyHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	return CallNextHookEx(0, nCode, wParam, lParam);
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-
-	CoInitialize(0);
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	CoInitialize(NULL);
 
 	WNDCLASSEX msgClass = {};
 	msgClass.cbSize = sizeof(WNDCLASSEX);
@@ -232,7 +250,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	msgClass.lpszClassName = msgClassName;
 
 	RegisterClassEx(&msgClass);
-	msgWindow = CreateWindowEx(0, msgClassName, "Netflix and Game", 0, 0, 0, 0, 0, HWND_MESSAGE, 0, hInstance, 0);
+	msgWindow = CreateWindowEx(0, msgClassName, "Netflix and Game", NULL, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
 	popupMenu = GetSubMenu(LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU1)), 0);
 
 	shellData = {};
@@ -247,7 +265,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	keyHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyHookProc, 0, 0);
 
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0) > 0) {
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
