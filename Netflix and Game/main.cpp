e96c6b7225a581ce55b94bd6642ed69d126db73c
Sloan Haywood <haywood.sloan@yahoo.com>
2018-05-13 01:50:23 -0700

    Use register hotkey rather than a key hook.

--- Netflix and Game/main.cpp
+++ Netflix and Game/main.cpp
@@ -22,84 +22,14 @@ struct mediaCommand
 	UINT button;
 };
 
-const mediaCommand mediaCommands[] = {{"YouTube",'K'}, {"Netflix", VK_SPACE}, {"Hulu", VK_SPACE},
-									  {"Spotify", NULL}, {"Skype", NULL}, {"VLC media player", VK_SPACE}, 
-									  {"Plex", VK_SPACE}, {"CW iFrame", VK_SPACE}, {"Amazon.com", VK_SPACE}};
-
-LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	if (uMsg == shellCallback && lParam == WM_RBUTTONUP)
-	{
-		POINT p;
-		GetCursorPos(&p);
-
-		SetForegroundWindow(msgWindow);
-		TrackPopupMenu(popupMenu, 0, p.x, p.y, 0, msgWindow, 0);
-		PostMessage(msgWindow, WM_NULL, 0, 0);
-	}
-	else if (uMsg == WM_COMMAND)
-	{
-		if (LOWORD(wParam) == quitItemID)
-		{
-			PostMessage(hwnd, WM_CLOSE, 0, 0);
-		}
-		else if (LOWORD(wParam) == reqFSItemID)
-		{
-			reqFullscreen = !reqFullscreen;
-
-			MENUITEMINFO checkInfo = {};
-			checkInfo.cbSize = sizeof(MENUITEMINFO);
-			checkInfo.fMask = MIIM_STATE;
-
-			GetMenuItemInfo(popupMenu, reqFSItemID, 0, &checkInfo);
-			checkInfo.fState ^= MFS_CHECKED;
-			checkInfo.fState ^= MFS_UNCHECKED;
-			SetMenuItemInfo(popupMenu, reqFSItemID, 0, &checkInfo);
-		}
-		else
-		{
-			soundOption = LOWORD(wParam);
-
-			MENUITEMINFO checkInfo = {};
-			checkInfo.cbSize = sizeof(MENUITEMINFO);
-			checkInfo.fMask = MIIM_STATE;
-
-			HMENU soundOptions = GetSubMenu(popupMenu, 0);
-			UINT count = GetMenuItemCount(soundOptions);
-
-			for (UINT i = 0; i < count; i++)
-			{
-				if (GetMenuItemID(soundOptions, i) == soundOption)
-				{
-					checkInfo.fState = MFS_CHECKED;
-					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
-				}
-				else
-				{
-					checkInfo.fState = MFS_UNCHECKED;
-					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
-				}
-			}
-		}
-	}
-	else if (uMsg == taskbarCreateMsg)
-	{
-		Shell_NotifyIcon(NIM_ADD, &shellData);
-	}
-	else if (uMsg == WM_CREATE)
-	{
-		taskbarCreateMsg = RegisterWindowMessage(TEXT("TaskbarCreated"));
-	}
-	else if (uMsg == WM_DESTROY)
-	{
-		PostQuitMessage(0);
-	}
-
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
+const mediaCommand mediaCommands[] = { {"YouTube",'K'}, {"Netflix", VK_SPACE}, {"Hulu", VK_SPACE},
+									  {"Spotify", NULL}, {"Skype", NULL}, {"VLC media player", VK_SPACE},
+									  {"Plex", VK_SPACE}, {"CW iFrame", VK_SPACE}, {"Amazon.com", VK_SPACE} };
 
 void changeFGWindowVolume()
 {
+	if (soundOption == dncItemID) return;
+
 	char titleBuff[128];
 	HWND activeHWND = GetForegroundWindow();
 	GetWindowText(activeHWND, titleBuff, 128);
@@ -178,7 +108,7 @@ void changeFGWindowVolume()
 	mmDeviceEnum->Release();
 }
 
-BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam)
+BOOL WINAPI pausePlayMediaEnumProc(HWND hwnd, LPARAM lParam)
 {
 	char titleBuff[128];
 	GetWindowText(hwnd, titleBuff, 128);
@@ -188,12 +118,8 @@ BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam)
 	{
 		if (strstr(titleBuff, mediaCommands[i].title) > 0)
 		{
-			bool& hasChangedVolume = *((bool*)lParam);
-			if (soundOption != dncItemID && !hasChangedVolume)
-			{
-				changeFGWindowVolume();
-				hasChangedVolume = true;
-			}
+			bool* hasChangedVolume = (bool*)lParam;
+			*hasChangedVolume = true;
 
 			LONG windowStyles = GetWindowLong(hwnd, GWL_EXSTYLE);
 			LONG windowStyleNoActive = windowStyles | WS_EX_NOACTIVATE;
@@ -210,6 +136,13 @@ BOOL WINAPI EnumWindowProc(HWND hwnd, LPARAM lParam)
 	}
 
 	return 1;
+}
+
+bool pausePlayMedia()
+{
+	bool hasChangedVolume = false;
+	EnumWindows(pausePlayMediaEnumProc, (LPARAM)&hasChangedVolume);
+	return hasChangedVolume;
 }
 
 bool isActiveWindowFullscreen()
@@ -232,33 +165,97 @@ bool isActiveWindowFullscreen()
 		activeHwndRect.right >= activeMonitorInfo.rcMonitor.right - 1);
 }
 
-LRESULT CALLBACK keyHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK msgClassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	if (wParam == WM_KEYDOWN)
+	if (uMsg == WM_HOTKEY)
 	{
-		KBDLLHOOKSTRUCT *kbHookStruct = (KBDLLHOOKSTRUCT*)lParam;
-		if (kbHookStruct->vkCode == VK_MEDIA_PLAY_PAUSE)
+		WORD key = HIWORD(lParam);
+		WORD mods = LOWORD(lParam);
+
+		if (key == VK_MEDIA_PLAY_PAUSE &&
+			(!reqFullscreen || isActiveWindowFullscreen()) &&
+			pausePlayMedia())
 		{
-			if (!reqFullscreen || isActiveWindowFullscreen())
+			changeFGWindowVolume();
+		}
+		else if (key == VK_F6 && (mods & MOD_ALT) &&
+			(!reqFullscreen || isActiveWindowFullscreen()))
+		{
+			changeFGWindowVolume();
+		}
+		else if (key == VK_F6 && (mods & MOD_CONTROL))
+		{
+			pausePlayMedia();
+		}
+	}
+	else if (uMsg == shellCallback && lParam == WM_RBUTTONUP)
+	{
+		POINT p;
+		GetCursorPos(&p);
+
+		SetForegroundWindow(msgWindow);
+		TrackPopupMenu(popupMenu, 0, p.x, p.y, 0, msgWindow, 0);
+		PostMessage(msgWindow, WM_NULL, 0, 0);
+	}
+	else if (uMsg == WM_COMMAND)
+	{
+		if (LOWORD(wParam) == quitItemID)
+		{
+			PostMessage(hwnd, WM_CLOSE, 0, 0);
+		}
+		else if (LOWORD(wParam) == reqFSItemID)
+		{
+			reqFullscreen = !reqFullscreen;
+
+			MENUITEMINFO checkInfo = {};
+			checkInfo.cbSize = sizeof(MENUITEMINFO);
+			checkInfo.fMask = MIIM_STATE;
+
+			GetMenuItemInfo(popupMenu, reqFSItemID, 0, &checkInfo);
+			checkInfo.fState ^= MFS_CHECKED;
+			checkInfo.fState ^= MFS_UNCHECKED;
+			SetMenuItemInfo(popupMenu, reqFSItemID, 0, &checkInfo);
+		}
+		else
+		{
+			soundOption = LOWORD(wParam);
+
+			MENUITEMINFO checkInfo = {};
+			checkInfo.cbSize = sizeof(MENUITEMINFO);
+			checkInfo.fMask = MIIM_STATE;
+
+			HMENU soundOptions = GetSubMenu(popupMenu, 0);
+			UINT count = GetMenuItemCount(soundOptions);
+
+			for (UINT i = 0; i < count; i++)
 			{
-				bool hasChangedVolume = false;
-				EnumWindows(EnumWindowProc, (LPARAM)&hasChangedVolume);
+				if (GetMenuItemID(soundOptions, i) == soundOption)
+				{
+					checkInfo.fState = MFS_CHECKED;
+					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
+				}
+				else
+				{
+					checkInfo.fState = MFS_UNCHECKED;
+					SetMenuItemInfo(popupMenu, GetMenuItemID(soundOptions, i), 0, &checkInfo);
+				}
 			}
 		}
 	}
-	else if (wParam == WM_SYSKEYDOWN)
+	else if (uMsg == taskbarCreateMsg)
 	{
-		KBDLLHOOKSTRUCT *kbHookStruct = (KBDLLHOOKSTRUCT*)lParam;
-		if (kbHookStruct->vkCode == VK_F6)
-		{
-			if (!reqFullscreen || isActiveWindowFullscreen() && soundOption != dncItemID)
-			{
-				changeFGWindowVolume();
-			}
-		}
+		Shell_NotifyIcon(NIM_ADD, &shellData);
+	}
+	else if (uMsg == WM_CREATE)
+	{
+		taskbarCreateMsg = RegisterWindowMessage(TEXT("TaskbarCreated"));
+	}
+	else if (uMsg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
 	}
 
-	return CallNextHookEx(0, nCode, wParam, lParam);
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -286,7 +283,9 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	strcpy_s(shellData.szTip, "Netflix and Game");
 
 	Shell_NotifyIcon(NIM_ADD, &shellData);
-	keyHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyHookProc, 0, 0);
+	RegisterHotKey(msgWindow, 0, MOD_NOREPEAT, VK_MEDIA_PLAY_PAUSE);
+	RegisterHotKey(msgWindow, 1, MOD_NOREPEAT | MOD_ALT, VK_F6);
+	RegisterHotKey(msgWindow, 2, MOD_NOREPEAT | MOD_CONTROL, VK_F6);
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
@@ -296,7 +295,5 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	}
 
 	Shell_NotifyIcon(NIM_DELETE, &shellData);
-	UnhookWindowsHookEx(keyHook);
-
 	return 0;
 }
